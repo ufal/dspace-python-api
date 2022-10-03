b32ec0387797d3b4689816bd4edb4e77121ab6c4
MajoBerger <marian.berger@dataquest.sk>
2022-10-03 13:51:23 +0200

    unified import licenses and labels

--- import_licenses_2_db.py
+++ import_licenses_2_db.py
@@ -2,11 +2,17 @@ import json
 
 from support.item_checking import import_license
 
-print('Going to import licenses.')
-# Opening JSON file
-with open('import/data/license_definitions.json') as json_file:
-    licenseDefinitions = json.load(json_file)
 
-    for license in licenseDefinitions:
-        import_license(license["name"], license["definition"], license["labelId"], license["confirmation"], license["requiredInfo"])
-        print(f'License: {license} imported!')
+def import_licenses():
+    try:
+        print('Going to import licenses.')
+        # Opening JSON file
+        with open('import/data/license_definitions.json') as json_file:
+            licenseDefinitions = json.load(json_file)
+
+            for license in licenseDefinitions:
+                import_license(license["name"], license["definition"], license["labelId"], license["confirmation"],
+                               license["requiredInfo"])
+                print(f'License: {license} imported!')
+    except:
+        print("import_licenses() failed")
---
+++ install_licenses.py
@@ -0,0 +1,6 @@
+import import_license_labels_2_db
+import import_licenses_2_db
+
+
+import_licenses_2_db.import_licenses()
+import_license_labels_2_db.import_license_labels()
