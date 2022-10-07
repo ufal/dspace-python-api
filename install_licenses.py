4d71b14d19fd52e4ed61fb15edb0b40c630ace8d
MilanMajchrák <milan.majchak@dataquest.sk>
2022-10-07 09:42:27 +0200

    create license definitions v2 file with more licenses

--- install_licenses.py
+++ install_licenses.py
@@ -1,6 +1,5 @@
 import import_license_labels_2_db
 import import_licenses_2_db
 
-
-import_licenses_2_db.import_licenses()
 import_license_labels_2_db.import_license_labels()
+import_licenses_2_db.import_licenses()
