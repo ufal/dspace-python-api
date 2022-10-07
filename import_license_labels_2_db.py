b6cc6a007101d3fbfe8cef46325a4c254c94c0a9
MilanMajchrák <milan.majchak@dataquest.sk>
2022-10-07 14:15:15 +0200

    fixed importing license labels and licenses

--- import_license_labels_2_db.py
+++ import_license_labels_2_db.py
@@ -1,18 +1,14 @@
 import json
-
 from support.item_checking import import_license_label
-
-
+ 
 def import_license_labels():
-    try:
-        print('Going to import license labels.')
-        # Opening JSON file
-        with open('import/data/license_labels.json') as json_file:
-            licenseLabelsJson = json.load(json_file)
+    print('Going to import license labels.')
+    # Opening JSON file
+    with open('import/data/license_labels.json') as json_file:
+        licenseLabelsJson = json.load(json_file)
 
-            for licenseLabel in licenseLabelsJson:
-                import_license_label(licenseLabel["id"], licenseLabel["label"], licenseLabel["title"],
-                                     licenseLabel["extended"])
-                print(f'License label: {licenseLabel} imported!')
-    except:
-        print("import_license_labels() failed")
+        for licenseLabel in licenseLabelsJson:
+            import_license_label(licenseLabel["id"], licenseLabel["label"], licenseLabel["title"], licenseLabel["extended"])
+            print(f'License label: {licenseLabel} imported!')
+
+
--- import_licenses_2_db.py
+++ import_licenses_2_db.py
@@ -1,18 +1,12 @@
 import json
-
 from support.item_checking import import_license
-
-
+ 
 def import_licenses():
-    try:
-        print('Going to import licenses.')
-        # Opening JSON file
-        with open('import/data/license_definitions.json') as json_file:
-            licenseDefinitions = json.load(json_file)
+    print('Going to import licenses.')
+    # Opening JSON file
+    with open('import/data/license_definitions_v2.json') as json_file:
+        licenseDefinitions = json.load(json_file)
 
-            for license in licenseDefinitions:
-                import_license(license["name"], license["definition"], license["labelId"], license["confirmation"],
-                               license["requiredInfo"])
-                print(f'License: {license} imported!')
-    except:
-        print("import_licenses() failed")
+        for license in licenseDefinitions:
+            import_license(license["name"], license["definition"], license["labelId"], license["confirmation"], license["requiredInfo"])
+            print(f'License: {license} imported!')
