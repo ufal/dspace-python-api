e2283ae90076ad72afba31077b60ba2c550ff74a
Michaela Paurikova <michaela.paurikova@dataquest.sk>
2023-02-16 16:17:27 +0100

    refactoring

--- data_import.py
+++ data_import.py
@@ -5,22 +5,34 @@ import psycopg2
 from support.dspace_proxy import rest_proxy
 
 eperson_id = dict()
+group_id = dict()
+metadata_field_id = dict()
 
-def import_license(request_mapping, file_name):
-    url = const.API_URL + request_mapping
-    x = open("C:/dspace-blackbox-testing/data/" + file_name)
-    json_array = json.load(x)
+# def import_license(request_mapping, file_name):
+#     url = const.API_URL + request_mapping
+#     x = open("C:/dspace-blackbox-testing/data/" + file_name)
+#     json_array = json.load(x)
+#     x.close()
+#     rest_proxy.d.api_post(url, None, json_array)
+
+# def import_licnses():
+#     test_data()
+#     import_license('licenses/import/labels', "license_label.json")
+#     import_license('licenses/import/extendedMapping', "license_label_extended_mapping.json")
+#     import_license('licenses/import/licenses', "license_definition.json")
+#     test_data()
+
+def import_communities(request_mapping, file_name, handle_file_name, metadata_file_name):
+    global metadata_field_id, group_id
+    #read handle
+    x = open("C:/dspace-blackbox-testing/data/" + handle_file_name)
+    handle_json = json.load(x)
     x.close()
-    rest_proxy.d.api_post(url, None, json_array)
+    #read metadata
+    x = open("C:/dspace-blackbox-testing/data/" + metadata_file_name)
+    metadata_json = json.load(x)
+    x.close()
 
-def import_licnses():
-    test_data()
-    import_license('licenses/import/labels', "license_label.json")
-    import_license('licenses/import/extendedMapping', "license_label_extended_mapping.json")
-    import_license('licenses/import/licenses', "license_definition.json")
-    test_data()
-
-def import_communities(request_mapping, file_name):
     url = const.API_URL + request_mapping
     x = open("C:/dspace-blackbox-testing/data/" + file_name)
     json_array = json.load(x)
@@ -28,8 +40,26 @@ def import_communities(request_mapping, file_name):
     if json_array is None:
         return
     for i in json_array:
-        json_data = {''}
-        response = rest_proxy.d.api_post(url, None, i)
+        #find handle
+        for j in handle_json:
+            if j['resource_type_id'] == 4 and j['resource_id'] == i['community_id']:
+                handle = j['handle']
+                break
+
+        #metadatafield mapping is not done
+        #find metadata
+        metadata = dict()
+        for j in metadata_json:
+            if j['resource_id'] == i['community_id']:
+                #get metadatafield
+                response1 = rest_proxy.d.api_get(const.API_URL + 'core/metadatafields/' + str(metadata_field_id[j['metadata_field_id']]))
+                #get metadatavalue
+                response2 = rest_proxy.d.api_get(
+                    const.API_URL + 'core/metadataschemas/' + str(json.loads(response2.content.decode('utf-8'))['schema']['id']))
+                metadata[json.loads(response2.content.decode('utf-8'))['short_id'] + '.' +
+                json.loads(response1.content.decode('utf-8'))['element'] + '.' + json.loads(response1.content.decode('utf-8'))['qualifier']] = {'value' : j['text_value'], 'language' : j['text_lang'], 'authority' : j['authority'], 'confidence' : j['confidence'], 'place' : j['place']}
+        json_data = {'handle' : handle, 'admin' : group_id[i['eperson_group_id']], 'metadata' : metadata}
+        response = rest_proxy.d.api_post(url, None, json_data)
         print(response)
 
 def import_bitstreamformat(request_mapping, file_name):
@@ -70,6 +100,7 @@ def import_metadataschemaRegistry(request_mapping, file_name):
         schema[i['metadata_schema_id']] = json.loads(response.content.decode('utf-8'))['id']
     import_metadataFieldRegistry('core/metadatafields', 'metadatafieldregistry.json', schema)
 def import_metadataFieldRegistry(request_mapping, file_name, schema):
+    global metadata_field_id
     url = const.API_URL + request_mapping
     x = open("C:/dspace-blackbox-testing/data/" + file_name)
     json_array = json.load(x)
@@ -80,6 +111,7 @@ def import_metadataFieldRegistry(request_mapping, file_name, schema):
         json_data = {'element' : i['element'], 'qualifier' : i['qualifier'], 'scopeNote' : i['scope_note']}
         param = {'schemaId': schema[i['metadata_schema_id']]}
         response = rest_proxy.d.api_post(url, param, json_data)
+        metadata_field_id[i['metadata_field_id']] = json.loads(response.content.decode('utf-8'))['id']
 
 def import_registrationdata(request_mapping, file_name):
     url = const.API_URL + request_mapping
@@ -133,16 +165,16 @@ def test_add_subgroup(request_mapping, file_name, mapping):
         response = rest_proxy.d.api_post(url + mapping[i['parent_id']] + "/subgroups", None, i)
         print(response)
 
-def test_schema_version(request_mapping, file_name):
-    url = const.API_URL + request_mapping
-    x = open("C:/dspace-blackbox-testing/data/" + file_name)
-    json_array = json.load(x)
-    x.close()
-    if json_array is None:
-        return
-    for i in json_array:
-        response = rest_proxy.d.api_post(url, None, i)
-        print(response)
+# def test_schema_version(request_mapping, file_name):
+#     url = const.API_URL + request_mapping
+#     x = open("C:/dspace-blackbox-testing/data/" + file_name)
+#     json_array = json.load(x)
+#     x.close()
+#     if json_array is None:
+#         return
+#     for i in json_array:
+#         response = rest_proxy.d.api_post(url, None, i)
+#         print(response)
 
 def test_eperson(request_mapping, file_name):
     global eperson_id
@@ -174,20 +206,28 @@ def test_eperson(request_mapping, file_name):
 
     #registratondata("eperson/registrations", "registrationdata.json")
 
-def registratondata(request_mapping, file_name):
-    global eperson_id
-    url = const.API_URL + request_mapping
-    x = open("C:/dspace-blackbox-testing/data/" + file_name)
-    json_array = json.load(x)
-    x.close()
-    if json_array is None:
-        return
-    for i in json_array:
-        json_data = {'user': i['user'], 'email' : i['email']}
-        response = rest_proxy.d.api_post(url, None, i)
+# def registratondata(request_mapping, file_name):
+#     global eperson_id
+#     url = const.API_URL + request_mapping
+#     x = open("C:/dspace-blackbox-testing/data/" + file_name)
+#     json_array = json.load(x)
+#     x.close()
+#     if json_array is None:
+#         return
+#     for i in json_array:
+#         json_data = {'user': i['user'], 'email' : i['email']}
+#         response = rest_proxy.d.api_post(url, None, i)
 
 def test_group(request_mapping, file_name1, file_name2):
+    global group_id
     url = const.API_URL + request_mapping
+
+    # get all existing groups from database
+    #response = rest_proxy.d.api_get(url)
+    #groups = json.loads(response.content.decode('utf-8'))['_embedded']['groups']
+    #group_dict = dict()
+    #for i in groups:
+    #    group_dict[i['name']] = i['id']
     x = open("C:/dspace-blackbox-testing/data/" + file_name1)
     json_array = json.load(x)
     x.close()
@@ -201,17 +241,16 @@ def test_group(request_mapping, file_name1, file_name2):
     x = open("C:/dspace-blackbox-testing/data/" + file_name2)
     json_array = json.load(x)
     x.close()
-    mapping = dict()
     #response = rest_proxy.d.api_post(url, None, i)
     for i in json_array:
         if i['eperson_group_id'] != 0 and i['eperson_group_id'] != 1:
             json_data = {'name': metadata[i['eperson_group_id']]}
             response = rest_proxy.d.api_post(url, None, json_data)
-            mapping[i['eperson_group_id']] = json.loads(response.content.decode('utf-8'))['id']
+            group_id[i['eperson_group_id']] = json.loads(response.content.decode('utf-8'))['id']
     print()
 
-    #subgrou[
-    test_add_subgroup('eperson/groups', 'group2group.json', mapping)
+    #subgrou
+    #test_add_subgroup('eperson/groups', 'group2group.json', group_id)
 
 
 #import_eperson()
@@ -252,10 +291,10 @@ def test_group(request_mapping, file_name1, file_name2):
 #         response = rest_proxy.d.api_post(url, None, i)
 #         mapping[i['versionhistory_id']] = json.loads(response.content.decode('utf-8'))['id']
 
-def import_schema_version():
-    test_data()
-    test_schema_version('versioning/versions', "schema_version.json")
-    test_data()
+# def import_schema_version():
+#     test_data()
+#     test_schema_version('versioning/versions', "schema_version.json")
+#     test_data()
 
 def test_data():
     conn = psycopg2.connect(database="dspace",
@@ -291,17 +330,19 @@ def import_collection():
 
 def import_group():
     test_group('eperson/groups', "metadatavalue.json", "epersongroup.json")
-import_community()
+
 #import_collection()
 #import_licnses()
 #import_schema_version()
 #import_eperson()
-#import_group()
+import_group()
 #import_metadata()
 
 #registratondata("eperson/registrations", "registrationdata.json")
 #import_items('core/items', 'item.json')
 #test_handle('core/handles', 'handle.json')
 #import_bitstreamformat('core/bitstreamformats', 'bitstreamformatregistry.json')
-#import_metadataschemaRegistry('core/metadataschemas', 'metadataschemaregistry.json')
+import_metadataschemaRegistry('core/metadataschemas', 'metadataschemaregistry.json')
 #import_registrationdata('eperson/registrations','registrationdata.json')
+
+import_communities('core/communities', 'community.json', 'handle.json', 'metadatavalue.json')
---
+++ dspace_import.py
@@ -0,0 +1,340 @@
+import json
+
+import const
+from support.logs import log
+from support.dspace_proxy import rest_proxy
+
+#global param
+eperson_id = dict()
+group_id = dict()
+metadata_schema_id = dict()
+metadata_field_id = dict()
+community_id = dict()
+metadatavalue = dict()
+handle = dict()
+
+#using functions
+def read_json(file_name):
+    """
+    Read data from file as json.
+    @param file_name: file name
+    @return: data as json
+    """
+    x = open("C:/dspace-blackbox-testing/data/" + file_name)
+    json_p = json.load(x)
+    x.close()
+    return json_p
+
+def do_api_post(url, param, json_p):
+    """
+    Insert data into database by api if they are not None.
+    @param url: url for api post
+    @param param: parameters for api post
+    @param json_p: posted data
+    @return: response from api post
+    """
+    url = const.API_URL + url
+    response = rest_proxy.d.api_post(url, param, json_p)
+    #control of responce status is missing
+    log('Api post by ' + url + ' was successfully done!')
+    return response
+
+def do_api_get_one(url, id):
+    """
+    Get data with id from table.
+    @param url: url for api get
+    @param id: id of object
+    @return: response from api get
+    """
+    url = const.API_URL + url + '/' + str(id)
+    response = rest_proxy.d.api_get(url, None, None)
+    #control of response status is missing
+    log('Api get by ' + url + ' was successfully done!')
+    return response
+
+def do_api_get_all(url):
+    """
+    Get all data from table.
+    @param url: url for api get
+    @return: response from api get
+    """
+    url = const.API_URL + url
+    param = {'size' : 1000}
+    response = rest_proxy.d.api_get(url, param, None)
+    #control of response satus is missing
+    log('Api get by url ' + url + ' was successfully done!')
+    return response
+
+def convert_response_to_json(response):
+    """
+    Convert response to json.
+    @param response: response from api call
+    @return: json created from response
+    """
+    return json.loads(response.content.decode('utf-8'))
+
+def get_metadata_by_type(resource_type_id):
+    """
+    Return metadata with resource_type_id.
+    @param resource_type_id: id of resource type
+    @return: dictionary resource_id : text_value
+    """
+    json_a = read_json('metadatavalue.json')
+    metadata = dict()
+    for i in json_a:
+        if i['resource_type_id'] == resource_type_id:
+            metadata[i['resource_id']] = i['text_value']
+    return metadata
+
+
+def read_metadata():
+    """
+    Read metadatavalue as json and
+    convert it to dictionary with tuple key: resource_type_id and resource_id.
+    """
+    global metadatavalue
+    metadatavalue_json = read_json('metadatavalue.json')
+    for i in metadatavalue_json:
+        key = (i['resource_type_id'], i['resource_id'])
+        if key in metadatavalue.keys():
+            metadatavalue[key].append(i)
+        else:
+            metadatavalue[key] = [i]
+
+def read_handle():
+    """
+    Read handle as json and convert it to dictionary wth tuple key: resource_type_id and resource_type,
+    where value is list of jsons.
+    """
+    global handle
+    handle_json = read_json('handle.json')
+    for i in handle_json:
+        key = (i['resource_type_id'], i['resource_id'])
+        if key in handle.keys():
+            handle[key].append(i)
+        else:
+            handle[key] = [i]
+
+def get_metadata_value(old_resource_type_id, old_resource_id):
+    """
+    Get metadata value for dspace object.
+    """
+    global metadata_field_id, metadatavalue
+    result = dict()
+    if not metadatavalue:
+        read_metadata()
+    #get all metadatavalue for object
+    metadatavalue_obj = metadatavalue[(old_resource_type_id, old_resource_id)]
+    if metadatavalue_obj:
+        #create list of object metadata
+        for i in metadatavalue_obj:
+            # get metadatafield
+            metadatafield_json = convert_response_to_json(
+                do_api_get_one('core/metadatafields', metadata_field_id[i['metadata_field_id']]))
+            # get metadataschema
+            metadataschema_json = convert_response_to_json(do_api_get_one('core/metadataschemas',
+                                                           metadatafield_json['_embedded']['schema']['id']))
+            key = metadataschema_json['prefix'] + '.' + metadatafield_json['element']
+            if metadatafield_json['qualifier'] != None:
+                key += '.' + metadatafield_json['qualifier']
+            result[key] = {'value' : i['text_value'], 'language' : i['text_lang'], 'authority' : i['authority'],
+                           'confidence' : i['confidence'], 'place' : i['place']}
+    return result
+
+#table mapping
+def import_licenses():
+    """
+    Import data into database.
+    Mapped tables: license_label, extended_mapping, license_definitions
+    """
+    files = ['license_label.json', 'license_label_extended_mapping.json', 'license_definition.json']
+    end_points = ['licenses/import/labels', 'licenses/import/extendedMapping', 'licenses/import/licenses']
+    for f,e in zip(files, end_points):
+        do_api_post(e, None, read_json(f))
+
+def import_registrationdata():
+    """
+    Import data into database.
+    Mapped tables: registrationdata
+    """
+    json_a = read_json('registrationdata.json')
+    for i in json_a:
+        json_p = {'email' : i['email']}
+        do_api_post('eperson/registrations', None, json_p)
+
+def import_epersongroup():
+    """
+    Import data into database.
+    Mapped tables: epersongroup
+    """
+    global group_id
+    metadata = get_metadata_by_type(6)
+    json_a = read_json('epersongroup.json')
+    # group Administrator and Anonymous already exist
+    # we need to remember their id
+    existing_data = convert_response_to_json(do_api_get_all('eperson/groups'))['_embedded']['groups']
+    for i in existing_data:
+        if i['name'] == 'Anonymous':
+            group_id[0] = i['id']
+        if i['name'] == 'Administrator':
+            group_id[1] = i['id']
+    for i in json_a:
+        # group Administrator and Anonymous already exist
+        if i['eperson_group_id'] != 0 and i['eperson_group_id'] != 1:
+            #get group metadata
+            metadata_group = get_metadata_value(6, i['eperson_group_id'])
+            json_p = {'name': metadata[i['eperson_group_id']], 'metadata' : metadata_group}
+            group_id[i['eperson_group_id']] = convert_response_to_json(do_api_post('eperson/groups', None, json_p))['id']
+
+def import_eperson():
+    """
+    Import data into database.
+    Mapped tables: eperson
+    """
+    global eperson_id
+    json_a = read_json('eperson.json')
+    for i in json_a:
+        json_p = {'selfRegistered': i['self_registered'], 'requireCertificate' : i['require_certificate'],
+                  'netid' : i['netid'], 'canLogIn' : i['can_log_in'], 'lastActive' : i['last_active'],
+                  'email' : i['email'], 'password' : i['password']}
+        eperson_id[i['eperson_id']] = convert_response_to_json(do_api_post('eperson/epersons', None, json_p))['id']
+
+def import_group2group():
+    """
+    Import data into database.
+    Mapped tables: group2group
+    """
+    global group_id
+    json_a = read_json('group2group.json')
+    for i in json_a:
+        do_api_post('eperson/groups/' + group_id[i['parent_id']] + '/subgroups', None, group_id[i['child_id']])
+
+def import_group2eperson():
+    """
+    Import data into database.
+    Mapped tables: epersongroup2eperson
+    """
+    global group_id, eperson_id
+    json_a = read_json('epersongroup2eperson.json')
+    for i in json_a:
+        do_api_post('eperson/groups/' + group_id[i['epersn_group_id'] + '/epersons'], None, eperson_id[i['eperson_id']])
+
+def import_metadataschemaregistry():
+    """
+    Import data into database.
+    Mapped tables: metadataschemaregistry
+    """
+    global metadata_schema_id
+    # get all existing data from database table
+    existing_data = convert_response_to_json(do_api_get_all('core/metadataschemas'))['_embedded']['metadataschemas']
+    json_a = read_json('metadataschemaregistry.json')
+    for i in json_a:
+        json_p = {'namespace' : i['namespace'], 'prefix' : i['short_id']}
+        #prefix has to be unique
+        try:
+            metadata_schema_id[i['metadata_schema_id']] = convert_response_to_json(do_api_post('core/metadataschemas', None, json_p))['id']
+        except:
+            for j in existing_data:
+                if j['prefix'] == i['short_id']:
+                    metadata_schema_id[i['metadata_schema_id']] = j['id']
+                    break
+
+
+def import_metadatafieldregistry():
+    """
+    Import data into database.
+    Mapped tables: metadatafieldregistry
+    """
+    global metadata_schema_id, metadata_field_id
+    existing_data = convert_response_to_json(do_api_get_all('core/metadatafields'))['_embedded']['metadatafields']
+    json_a = read_json('metadatafieldregistry.json')
+    counter = 0
+    for i in json_a:
+        counter += 1
+        json_p = {'element' : i['element'], 'qualifier' : i['qualifier'], 'scopeNote' : i['scope_note']}
+        param = {'schemaId': metadata_schema_id[i['metadata_schema_id']]}
+        #element and qualifier have to be unique
+        try:
+            metadata_field_id[i['metadata_field_id']] = convert_response_to_json(do_api_post('core/metadatafields', param, json_p))['id']
+            print(counter)
+        except:
+            for j in existing_data:
+                if j['element'] == i['element'] and j['qualifier'] == i['qualifier']:
+                    metadata_field_id[i['metadata_field_id']] = j['id']
+                    print(counter)
+                    break
+
+
+
+def import_community():
+    """
+    Import data into database.
+    Mapped tables: community, community2community, metadatavalue
+    """
+    global group_id, metadatavalue, handle
+    if not handle:
+        read_handle()
+    if not metadatavalue:
+        read_metadata()
+
+    #json_comm2comm = read_json('community2community.json')
+    json_comm = read_json('community.json')
+
+    for i in json_comm:
+        #resource_type_id for community is 4
+        handle_comm = handle[(4,i['community_id'])]
+        metadatavalue_comm = metadatavalue[(4, i['community_id'])]
+        admin_comm = group_id[i['eperson_group_id']]
+        json_p = {'handle' : handle_comm, 'admin' : admin_comm, 'metadata' : metadatavalue_comm}
+        community_id[i['community_id']] = convert_response_to_json(do_api_post('core/communities', None, json_p))['id']
+
+def import_handle_with_url():
+    """
+    Import handles into database with url.
+    Other handles are imported by dspace objects.
+    Mapped table: handles
+    """
+    global handle
+    #if handle is empty, read handle file as json
+    if not handle:
+        read_handle()
+    #handle with defined url has key (None, None)
+    handles_url = handle[(None, None)]
+    for i in handles_url:
+        json_p = {'handle': i['handle'], 'url': i['url']}
+        do_api_post('core/handles', None, json_p)
+
+def import_epersons_and_groups():
+    """
+    Import part of dspace: epersons and groups.
+    """
+    import_registrationdata()
+    import_epersongroup()
+    import_group2group()
+    import_eperson()
+    import_group2eperson()
+    #missing epersongroup2workspace
+
+def import_metadata():
+    """
+    Import part of dspace: metadata
+    """
+    import_metadataschemaregistry()
+    import_metadatafieldregistry()
+
+def import_hierarchy():
+    """
+    Import part of dspace: hierarchy
+    """
+    import_community()
+
+
+#call
+#import_licenses()
+#import_registrationdata()
+#import_handle_with_url()
+
+#you have to call together
+import_metadata()
+import_epersons_and_groups()
+import_hierarchy()
