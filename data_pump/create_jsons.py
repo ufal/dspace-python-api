e888941d1db62d1812dccaf6840c416f5d2dea36
Paurikova2 <michaela.paurikova@dataquest.sk>
2023-06-29 13:23:55 +0200

    fix errors

--- data_pump/create_jsons.py
+++ data_pump/create_jsons.py
@@ -1,36 +1,44 @@
+import argparse
+import logging
+
 import psycopg2
 import json
-import sys
 
 
 def get_data_as_json(database, host, db_user, db_password):
-
     # create database connection
     conn = psycopg2.connect(database=database,
                             host=host,
                             user=db_user,
                             password=db_password)
-    print("Connection was successful!")
+    logging.info("Connection was successful!")
 
     cursor = conn.cursor()
     cursor.execute(
         "SELECT table_name FROM information_schema.tables WHERE is_insertable_into = 'YES' AND table_schema = 'public'")
     # list of tuples
     table_name = cursor.fetchall()
-    print("Processing...")
+    logging.info("Processing...")
     for name_t in table_name:
         # access to 0. position, because name_t is tuple
         name = name_t[0]
-        fp_name = 'data/' + name + '.json'
-        fp = open(fp_name, 'w')
-        cursor.execute("SELECT json_agg(row_to_json(t)) FROM {} t".format(name))
-        # access to 0. position, because the fetchone returns tuple
-        fp.write((json.dumps(cursor.fetchone()[0])))
-        fp.close()
-    print("Data was successfully exported!")
+        j_name = 'data/' + name + '.json'
+        with open(j_name, 'w', encoding='utf-8') as j:
+            cursor.execute("SELECT json_agg(row_to_json(t)) FROM \"{}\" t".format(name))
+            # access to 0. position, because the fetchone returns tuple
+            j.write((json.dumps(cursor.fetchone()[0])))
+    logging.info("Data was successfully exported!")
     conn.close()
-    print("Disconnect from database!")
+    logging.info("Disconnect from database!")
 
 
 if __name__ == "__main__":
-    get_data_as_json(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    parser = argparse.ArgumentParser(description='Process database connection')
+    parser.add_argument('--database', description='database name',
+                        required=True, type=str)
+    parser.add_argument('--host', description='type of host', required=True, type=str)
+    parser.add_argument('--user', description='database user', required=True, type=str)
+    parser.add_argument('--password', description='database password',
+                        required=True, type=str)
+    args = parser.parse_args()
+    get_data_as_json(args.database, args.host, args.user, args.password)
