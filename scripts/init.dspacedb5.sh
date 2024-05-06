0f097a150fe174fd894b0dc5b23bef02fe6a5cd0
milanmajchrak <90026355+milanmajchrak@users.noreply.github.com>
2024-05-06 09:26:43 +0200

    Do not specify the dump name in the script, manage this dump in the dev5 (#134)

--- scripts/init.dspacedb5.sh
+++ scripts/init.dspacedb5.sh
@@ -8,13 +8,13 @@ createuser --username=postgres dspace
 
 echo "Importing clarin-dspace"
 createdb --username=postgres --owner=dspace --encoding=UNICODE clarin-dspace
-psql -U postgres clarin-dspace < ../dump/clarin-dspace-8.8.23.sql &> /dev/null
-psql -U postgres clarin-dspace < ../dump/clarin-dspace-8.8.23.sql &> ./__clarin-dspace.log
+psql -U postgres clarin-dspace < ../dump/clarin-dspace.sql &> /dev/null
+psql -U postgres clarin-dspace < ../dump/clarin-dspace.sql &> ./__clarin-dspace.log
 
 echo "Importing clarin-utilities"
 createdb --username=postgres --encoding=UNICODE clarin-utilities
-psql -U postgres clarin-utilities < ../dump/clarin-utilities-8.8.23.sql &> /dev/null
-psql -U postgres clarin-utilities < ../dump/clarin-utilities-8.8.23.sql &> ./__clarin-utilities.log
+psql -U postgres clarin-utilities < ../dump/clarin-utilities.sql &> /dev/null
+psql -U postgres clarin-utilities < ../dump/clarin-utilities.sql &> ./__clarin-utilities.log
 
 echo "Done, starting psql"
 
